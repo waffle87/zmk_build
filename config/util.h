a534e8490f11209b286561c80fd9019e6380aa74
jack <0x6A73@pm.me>
2022-07-28 13:52:20 -0600

    add layer

--- config/util.h
+++ config/util.h
@@ -1,7 +1,6 @@
-// Copyright 2022 @waffle87
+// Copyright 2022 jack (@waffle87)
 // SPDX-License-Identifier: MIT
 
-compatible = "zmk,combos";
 #define COMBO(name, keypress, keypos) \
 combo_##name { \
   timeout-ms = <50>; \
