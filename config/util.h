9714fc5047026416d5afabda9f122b02101521b9
jack <0x6A73@pm.me>
2022-07-28 13:20:45 -0600

    clean up

---
+++ config/util.h
@@ -0,0 +1,13 @@
+// Copyright 2022 @waffle87
+// SPDX-License-Identifier: MIT
+
+compatible = "zmk,combos";
+#define COMBO(name, keypress, keypos) \
+combo_##name { \
+  timeout-ms = <50>; \
+  bindings = <keypress>; \
+  key-positions = <keypos>; \
+};
+
+#define HRML(k1,k2,k3,k4) &hm LALT k1 &hm LGUI k2 &hm LCTRL k3 &hm LSHFT k4
+#define HRMR(k1,k2,k3,k4) &hm RSHFT k1 &hm RCTRL k2 &hm RGUI k3 &hm RALT k4
