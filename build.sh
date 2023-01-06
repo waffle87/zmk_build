736812340fbb675f582747308ae347c382ada023
jack <0x6A73@pm.me>
2023-01-06 14:39:20 -0700

    fixup build script & add tap dance macros

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 ZSDK_VERSION=0.13.2
-ZEN_BRANCH=Board-Corne-ish-Zen-dedicated-work-queue
+WEST_CMD="west build -p -b"
 LINK=~/waffle_git/zmk/app/boards/shields/revxlp
 CONFIG_DIR=~/waffle_git/zmk-build/config
 ZMK_DIR=~/waffle_git/zmk
-UF2=~/*.uf2
 
 update() {
   (cd ${ZMK_DIR} ; git pull ; west update)
@@ -12,12 +11,6 @@ update() {
   chmod +x zephyr-toolchain-arm-${ZSDK_VERSION}-linux-x86_64-setup.run
   ./zephyr-toolchain-arm-${ZSDK_VERSION}-linux-x86_64-setup.run -- -d ~/.local/zephyr-sdk-${ZSDK_VERSION}
   rm zephyr-toolchain-arm-${ZSDK_VERSION}-linux-x86_64-setup.run
-}
-
-test_uf2() {
-  if test -f ${UF2}; then
-    rm ${UF2}
-  fi
 }
 
 read -p "update? (y/n) " -n 1 -r
@@ -28,67 +21,46 @@ fi
 
 echo -e "\n"
 PS3="choose keyboard to build: "
-options=("corne" "corne-ish zen" "revxlp" "settings reset" "quit")
+options=("corne" "revxlp" "settings reset" "quit")
 select opt in "${options[@]}"
 do
   case $opt in
     "corne")
       echo "building corne firmware..."
-      test_uf2
-      (cd ${ZMK_DIR}
-      if [ $(git branch --show-current) == "${ZEN_BRANCH}"]; then
-        git checkout main
-        update
-      fi)
       (cd ${ZMK_DIR}/app
-      west build -p -b nice_nano -- -DSHIELD=corne_left -DZMK_CONFIG="${CONFIG_DIR}"
+      ${WEST_CMD} nice_nano -- -DSHIELD=corne_left -DZMK_CONFIG="${CONFIG_DIR}" 1> /dev/null
       cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/corne_left.uf2
-      west build -p -b nice_nano_v2 -- -DSHIELD=corne_right -DZMK_CONFIG="${CONFIG_DIR}"
+      ${WEST_CMD} nice_nano_v2 -- -DSHIELD=corne_right -DZMK_CONFIG="${CONFIG_DIR}" 1> /dev/null
       cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/corne_right.uf2
       )
-      break
-      ;;
-    "corne-ish zen")
-      echo "building corne-ish zen firmware..."
-      test_uf2
-      cp config/corne.keymap config/corne-ish_zen.keymap
-      (cd ${ZMK_DIR}
-      if [[ $(git branch --show-current) = "main" ]]; then
-        git checkout ${ZEN_BRANCH}
-        update
-      fi)
-      (cd ${ZMK_DIR}/app
-      west build -p -b corne-ish_zen_left -- -DZMK_CONFIG="${CONFIG_DIR}"
-      cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/corne-ish_zen_left.uf2
-      west build -p -b corne-ish_zen_right -- -DZMK_CONFIG="${CONFIG_DIR}"
-      cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/corne-ish_zen_right.uf2
-      )
-      rm config/corne-ish_zen.keymap
+      echo "complete :^)"
       break
       ;;
     "revxlp")
       echo "building revxlp firmware..."
-      test_uf2
       cp config/corne.keymap revxlp/revxlp.keymap
       if ! [ -L $LINK]; then
         ln -s ~/waffle_git/zmk-build/revxlp $LINK
       fi
       (cd ${ZMK_DIR}/app
-      west build -p -b seeeduino_xiao_ble -- -DSHIELD=revxlp
+      ${WEST_CMD} seeeduino_xiao_ble -- -DSHIELD=revxlp 1> /dev/null
       cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/revxlp.uf2
       )
       rm revxlp/revxlp.keymap
+      echo "complete :^)"
       break
       ;;
     "settings reset")
       echo "building settings reset firmware..."
-      test_uf2
       (cd ${ZMK_DIR}/app
-      west build -p -b nice_nano -- -DSHIELD=settings_reset
+      ${WEST_CMD} nice_nano -- -DSHIELD=settings_reset &> /dev/null
       cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/settings_reset_v1.uf2
-      west build -p -b nice_nano_v2 -- -DSHIELD=settings_reset
+      ${WEST_CMD} nice_nano_v2 -- -DSHIELD=settings_reset &> /dev/null
       cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/settings_reset_v2.uf2
+      ${WEST_CMD} seeeduino_xiao_ble -- -DSHIELD=settings_reset &> /dev/null
+      cp ${ZMK_DIR}/app/build/zephyr/zmk.uf2 ~/settings_reset_xiao.uf2
       )
+      echo "complete :^)"
       break
       ;;
     "quit")
--- util.h
+++ util.h
@@ -2,11 +2,20 @@
 // SPDX-License-Identifier: MIT
 
 #define COMBO(name, keypress, keypos, layer) \
-combo_##name { \
-   layers = <layer>; \
-   timeout-ms = <50>; \
-   bindings = <keypress>; \
-   key-positions = <keypos>; \
+combo_##name {                               \
+   layers = <layer>;                         \
+   timeout-ms = <50>;                        \
+   bindings = <keypress>;                    \
+   key-positions = <keypos>;                 \
+};
+
+#define TAP_DANCE(name, keypress1, keypress2, hash) \
+td_##name: name {                                   \
+  compatible = "zmk,behavior-tap-dance";            \
+  label = "name";                                   \
+  hash##binding-cells = <0>;                        \
+  tapping-term-ms = <200>;                          \
+  bindings = <keypress1>, <keypress2>;              \
 };
 
 #define HRML(k1,k2,k3,k4) &hm LALT k1 &hm LGUI k2 &hm LCTRL k3 &hm LSHFT k4
