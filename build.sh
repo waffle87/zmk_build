74139b7f24e2427fb189350ebb1f69836fe76a03
Jack Sangdahl <0x6A73@pm.me>
2024-03-19 11:04:23 -0600

    add corneish zen

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ build() {
     (cd $ZMK_DIR/app
         MODULE_CMD=""
         if [ -n $3 ]; then
-            MODULE_CMD=-DZMK_EXTRA_MODULES="$HOME/git/zmk-build/$3"
+            MODULE_CMD="-DZMK_EXTRA_MODULES=$HOME/git/zmk-build/$3"
         fi
         west build -p always -b $2 -- -DSHIELD=$1 \
             -DZMK_CONFIG=$HOME/git/zmk-build/config $MODULE_CMD
@@ -35,7 +35,7 @@ build() {
 }
 
 printf "${BOLD}firmware to build${NORMAL}...\n"
-printf "(1) corne\t(2) revxlp\t(3) sweep\t(4) settings reset:\t"
+printf "(1) corne\t(2) corne-ish zen\t(3) revxlp\t(4) sweep\t(5) settings reset:\t"
 read opt;
 case $opt in
     1)
@@ -43,13 +43,17 @@ case $opt in
         build corne_right nice_nano_v2
         ;;
     2)
-        build revxlp seeeduino_xiao_ble "revxlp"
+        build corneish_zen_v1_left
+        build corneish_zen_v1_right
         ;;
     3)
+        build revxlp seeeduino_xiao_ble "revxlp"
+        ;;
+    4)
         build splitkb_aurora_sweep_left nice_nano_v2
         build splitkb_aurora_sweep_right nice_nano_v2
         ;;
-    4)
+    5)
         printf "${BOLD}select microcontroller${NORMAL}...\n"
         printf "(1) nice nano\t(2) nice nano v2\t(3) xiao ble:\t"
         read val;
