a9a7fd4ec81e1c3acb6de53aa5aa583df1f8ce40
Jack Sangdahl <jack@pngu.org>
2026-02-08 13:27:35 -0700

    Remove some unused functionalities, add zmk-helpers submodule

--- config/util.h
+++ config/util.h
@@ -9,14 +9,6 @@ combo_##name {                        \
    key-positions = <keypos>;          \
 };
 
-#define TAP_DANCE(name, kp1, kp2, kp3)   \
-td_##name: name {                        \
-  compatible = "zmk,behavior-tap-dance"; \
-  #binding-cells = <0>;                  \
-  tapping-term-ms = <200>;               \
-  bindings = <kp1>, <kp2>, <kp3>;        \
-};
-
 #define HRML(k1,k2,k3,k4) &hm LALT k1 &hm LGUI k2 &hm LCTRL k3 &hm LSHFT k4
 #define HRMR(k1,k2,k3,k4) &hm RSHFT k1 &hm RCTRL k2 &hm RGUI k3 &hm RALT k4
 
@@ -41,7 +33,6 @@ td_##name: name {                        \
       tapping-term-ms = <210>;
       bindings = <&kp C_PLAY>, <&kp C_NEXT>, <&kp C_PREV>;
     };
-    TAP_DANCE(min_dash, &kp MINUS, &dbl_min, &emdash)
   };
 
   macros {
@@ -69,19 +60,6 @@ td_##name: name {                        \
       compatible = "zmk,behavior-macro";
       #binding-cells = <0>;
       bindings = <&macro_tap &kp DLLR  &kp UNDER>;
-    };
-    dbl_min: dbl_min {
-      compatible = "zmk,behavior-macro";
-      #binding-cells = <0>;
-      bindings = <&macro_tap &kp MINUS &kp MINUS>;
-    };
-    emdash: emdash {
-      compatible = "zmk,behavior-macro";
-      #binding-cells = <0>;
-      bindings = <&macro_press &kp LSHFT &kp LCTRL>,
-                 <&macro_tap &kp U &kp N2 &kp N0 &kp N1 &kp N4>,
-                 <&macro_release &kp LSHFT &kp LCTRL>,
-                 <&macro_tap &kp SPACE &kp SPACE>;
     };
   };
 };
